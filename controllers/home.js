37632ce973f1344154a1851b2ead1fd5da22bac1
VasilVasilev91 <vasil_vasilev91@yahoo.com>
2017-04-27 23:34:08 +0300

    Edited error message appearance using bootstrap classes.

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,7 @@ const Article = mongoose.model('Article');
 
 module.exports = {
     index: (req, res) => {
-        Article.find({}).limit(6).populate('author').then(articles => {
+        Article.find({}).limit(25).populate('author').then(articles => {
             for (let article = 0; article < articles.length; article++) {
                 let trimmedString =  articles[article].content.substring(0,300);
                 articles[article].content = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
